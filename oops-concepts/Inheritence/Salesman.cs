b19fb2c5c0619417ca36184eb3d1d0279d0e37e5
hitman-stack <45466709+hitman-stack@users.noreply.github.com>
2020-10-17 02:04:18 +0530

    Abstract class

--- oops-concepts/Inheritence/Salesman.cs
+++ oops-concepts/Inheritence/Salesman.cs
@@ -1,5 +1,6 @@
 using System;
-public class CarSalesman
+
+public abstract class Salesman
 {
     //fields are started with _abc
     private string _firstName; //Keep your fields private 
@@ -11,14 +12,15 @@ public class CarSalesman
             return string.Format("{0}{1}",this._firstName,this._lastName);
         }
     }
-    public CarSalesman(string firstName,string lastName)
+    public Salesman(string firstName,string lastName)
     {
 this._firstName=firstName;
 this._lastName=lastName;
     }
 
-    public void Sell()
+    public virtual void Sell()
     {
-       Console.WriteLine(string.Format("Hi my name is {0}. I would like you to buy this car!",this.FullName));
-    }
+        Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to but this item!", this.FullName));
+    }    
+    
 }
